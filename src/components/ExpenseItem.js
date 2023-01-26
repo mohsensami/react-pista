2b1ec2f04d89f6c19e69ca37ea2a4c4e285da802
MohsenSami <mohsensami88@gmail.com>
2023-01-26 14:25:45 +0330

    change title by useState hook

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,24 @@
-import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate';
-import Card from './card';
+import React, { useState } from "react";
 
-const ExpenseItem = (props)=> {
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./card";
+
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+    const changetitle = () => {
+        setTitle("Title Changed");
+    };
     return (
-        <Card className='expense-item'>
+        <Card className="expense-item">
             <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-                <h3>{props.title}</h3>
-                <div className='expense-item__price'>${props.amount}</div>
+            <div className="expense-item__description">
+                <h3>{title}</h3>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={changetitle}>Change Title</button>
         </Card>
-    )
-}
+    );
+};
 
 export default ExpenseItem;
