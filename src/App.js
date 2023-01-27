34754b429a851119b05a92f880d9fed3d0d90006
MohsenSami <mohsensami88@gmail.com>
2023-01-27 20:09:18 +0330

    expence to own branch expense

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+const DUMMY_EXPENSES = [
+    {
+        id: "e1",
+        title: "Toilet Paper",
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+        id: "e3",
+        title: "Car Insurance",
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: "e4",
+        title: "New Desk (Wooden)",
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+    {
+        id: "e5",
+        title: "Pen (Wooden)",
+        amount: 350,
+        date: new Date(2022, 1, 12),
+    },
+];
 
-    useEffect(() => {
-        const loginStatus = localStorage.getItem("isLoggedIn");
-        if (loginStatus) {
-            setIsLoggedIn(true);
-        }
-    }, []);
+const App = () => {
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-    const loginHandler = (email, password) => {
-        localStorage.setItem("isLoggedIn", "1");
-        setIsLoggedIn(true);
-    };
-
-    const logoutHandler = () => {
-        localStorage.removeItem("isLoggedIn");
-        setIsLoggedIn(false);
+    const addExpenseHandler = (expense) => {
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses];
+        });
     };
 
     return (
-        <React.Fragment>
-            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-            <main>
-                {!isLoggedIn && <Login onLogin={loginHandler} />}
-                {isLoggedIn && <Home onLogout={logoutHandler} />}
-            </main>
-        </React.Fragment>
+        <div>
+            <NewExpense onAddExpense={addExpenseHandler} />
+            <Expenses items={expenses} />
+        </div>
     );
-}
+};
 
 export default App;
