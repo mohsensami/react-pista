8dc1a41e88bd9601ce70b2e17a89e90161f4cc6d
MohsenSami <mohsensami88@gmail.com>
2023-02-04 12:16:33 +0330

    update project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React, { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvide";
+import React from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
-    const [cartIsShown, setCartIsShown] = useState(false);
+  const dummyMovies = [
+    {
+      id: 1,
+      title: 'Some Dummy Movie',
+      openingText: 'This is the opening text of the movie',
+      releaseDate: '2021-05-18',
+    },
+    {
+      id: 2,
+      title: 'Some Dummy Movie 2',
+      openingText: 'This is the second opening text of the movie',
+      releaseDate: '2021-05-19',
+    },
+  ];
 
-    const showCartHandler = () => {
-        setCartIsShown(true);
-    };
-    const hideCartHandler = () => {
-        setCartIsShown(false);
-    };
-    return (
-        <CartProvider>
-            {cartIsShown && <Cart onClose={hideCartHandler} />}
-            <Header onShowCart={showCartHandler} />
-            <main>
-                <Meals />
-            </main>
-        </CartProvider>
-    );
+  return (
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default App;
