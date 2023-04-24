648e052199889585796365b402ffc8ef31355805
MohsenSami <mohsensami88@gmail.com>
2023-04-24 23:36:34 +0330

    images added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-import mealsImage from "../../assets/meals.jpg";
-import HeaderCartButton from "./HeaderCartButton";
-import classes from "./Header.module.css";
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+import classes from './Header.module.css';
 
 const Header = (props) => {
     return (
@@ -11,9 +11,9 @@ const Header = (props) => {
                 <h1>Shopping Cart</h1>
                 <HeaderCartButton onClick={props.onShowCart} />
             </header>
-            <div className={classes["main-image"]}>
+            {/* <div className={classes["main-image"]}>
                 <img src={mealsImage} alt="A table full of Foods!" />
-            </div>
+            </div> */}
         </React.Fragment>
     );
 };
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -4,29 +4,31 @@ import MealItemForm from './MealItemForm';
 import CartContext from './../../../store/cart-context';
 
 const MealItem = (props) => {
-
     const cartCtx = useContext(CartContext);
 
-    const price = `$${props.price.toFixed(2)}`
+    const price = `$${props.price.toFixed(2)}`;
 
-    const addToCartHandler = amount => {
+    const addToCartHandler = (amount) => {
         cartCtx.addItem({
             id: props.id,
             name: props.name,
             amount: amount,
-            price: props.price
+            price: props.price,
         });
     };
     return (
         <li className={classes.meal}>
             <div>
                 <h3>{props.name}</h3>
+                <div>
+                    <img src={props.image} />
+                </div>
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{price}</div>
             </div>
             <MealItemForm onAddToCart={addToCartHandler} />
         </li>
-    )
-}
+    );
+};
 
 export default MealItem;
