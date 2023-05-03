f54e5dfb6bdb88d0e62b937c8832991c14866acb
MohsenSami <mohsensami88@gmail.com>
2023-05-03 22:50:59 +0330

    svg loading component added

---
+++ src/components/UI/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Loading1 from '../../assets/loading.svg';
+
+function Loading() {
+    return (
+        <div>
+            <img src={Loading1} alt="loading" />
+        </div>
+    );
+}
+
+export default Loading;
