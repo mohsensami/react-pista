bf5a9cba078c50c690a619c766d9148b2f16b4f0
MohsenSami <mohsensami88@gmail.com>
2023-05-05 22:25:26 +0330

    modal added for show products

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,37 +5,6 @@ import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import { useEffect, useState } from 'react';
 
-// const DUMMY_MEALS = [
-//     {
-//         id: 'm1',
-//         name: 'Sushi',
-//         image: 'https://loremflickr.com/300/150/meal?lock=122',
-//         description: 'Finest fish and veggies',
-//         price: 22.99,
-//     },
-//     {
-//         id: 'm2',
-//         name: 'Schnitzel',
-//         image: 'https://loremflickr.com/300/150/meal?lock=65',
-//         description: 'A german specialty!',
-//         price: 16.5,
-//     },
-//     {
-//         id: 'm3',
-//         name: 'Barbecue Burger',
-//         image: 'https://loremflickr.com/300/150/meal?lock=42',
-//         description: 'American, raw, meaty',
-//         price: 12.99,
-//     },
-//     {
-//         id: 'm4',
-//         name: 'Green Bowl',
-//         image: 'https://loremflickr.com/300/150/meal?lock=12',
-//         description: 'Healthy...and green...',
-//         price: 18.99,
-//     },
-// ];
-
 const AvailableMeals = () => {
     const [products, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,12 @@
 import classes from './MealItem.module.css';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import MealItemForm from './MealItemForm';
 import CartContext from './../../../store/cart-context';
+import Modal from '../../UI/Modal';
 
 const MealItem = (props) => {
     const cartCtx = useContext(CartContext);
+    const [showModal, setShowModal] = useState(false);
 
     const price = `$${props.price.toFixed(2)}`;
 
@@ -16,10 +18,21 @@ const MealItem = (props) => {
             price: props.price,
         });
     };
+    const modalHandler = (id) => {
+        console.log(id);
+        return setShowModal((prev) => !prev);
+    };
     return (
         <li className={classes.meal}>
             <div>
-                <h3>{props.name}</h3>
+                <h3 onClick={() => modalHandler(props.id)}>{props.name}</h3>
+                {showModal && (
+                    <Modal>
+                        <div className={classes.actions}>
+                            <button onClick={() => setShowModal(false)}>Close</button>
+                        </div>
+                    </Modal>
+                )}
                 <div>
                     <img src={props.image} alt={props.title} width="200" />
                 </div>
