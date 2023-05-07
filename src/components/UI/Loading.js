14fd734d6ae46c35de110a8e17e6ff6858721cb4
MohsenSami <mohsensami88@gmail.com>
2023-05-07 21:34:51 +0330

    center loading

--- src/components/UI/Loading.js
+++ src/components/UI/Loading.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Laoding.css';
 
 import Loading1 from '../../assets/loading.svg';
 
 function Loading() {
     return (
-        <div>
+        <div className="loading">
             <img src={Loading1} alt="loading" />
         </div>
     );
